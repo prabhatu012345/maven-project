f50be4b8d93439d2c63b842216d6aebcec28ec75
Prabhat Kumar <44310215+prabhatu012345@users.noreply.github.com>
2024-09-02 12:23:17 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestSuite;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HelloWorldTest {
+public class AppTest.java {
 
     @Test
     public void testGreet() {
