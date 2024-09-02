14b394334529d26d014df7028ec3758b98c44fdd
Prabhat Kumar <44310215+prabhatu012345@users.noreply.github.com>
2024-09-02 12:26:09 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,17 +1,32 @@
-package com.example;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
 
-public class AppTest {
-
-    @Test
-    public void testGreet() {
-        HelloWorld helloWorld = new HelloWorld();
-        String result = helloWorld.greet("World");
-        assertEquals("Hello, World!", result);
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
